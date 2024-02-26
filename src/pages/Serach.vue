0c747530221f1f69f2997afbd969ee4b4d0a9025
DamianoL <damianolunardo@hotmail.it>
2024-02-26 14:25:50 +0100

    Add - error if no pokemon name exsist

--- src/pages/Serach.vue
+++ src/pages/Serach.vue
@@ -2,14 +2,22 @@
 import axios from 'axios';
 import { ref } from 'vue';
 
+//la variabile search la definiamo come riferimento con ref e inizializzata a un valore vuoto
 const search = ref('');
+// gestisco il caso in cui il nome del pokemon non esiste
+const errorMessage = ref('');
 
 const searchPokemon = async () => {
   try {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search.value}`);
-    console.log(response.data); // Stampiamo i dati del Pokémon nella console
+    // Stampiamo i dati del Pokémon nella console
+    console.log(response.data); 
+    // inizialmente vuoto
+    errorMessage.value = ''; 
   } catch (error) {
     console.error('Nessun pokemon trovato:', error);
+    // messaggio di errore a schermo
+    errorMessage.value = 'Nessuna pokemon trovato.';
   }
 };
 </script>
@@ -18,9 +26,10 @@ const searchPokemon = async () => {
   <div>
     <input type="text" v-model="search" placeholder="Ricerca il pokemon">
     <button @click="searchPokemon">Search</button>
+    <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
   </div>
 </template>
 
 <style scoped>
-/* Aggiungi stili CSS se necessario */
+
 </style>
--- src/store.js
+++ src/store.js
@@ -3,8 +3,10 @@ import { reactive } from "vue"
 const store = reactive({
     BASE_URL: 'https://pokeapi.co/api/v2/pokemon/{id or name}/',
     pokemons: [],
+    search: '',
     
     get url() {
+        // sostituisco la ricerca con l'id o il nome   
         return this.BASE_URL.replace('{id or name}', this.search)
     }
 
