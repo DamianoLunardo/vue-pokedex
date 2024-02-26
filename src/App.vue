bf7b5d5f3108bf215a6bb53c6214c24012c9768d
DamianoL <damianolunardo@hotmail.it>
2024-02-26 12:29:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWord from './components/HelloWorld.vue'
+
+</script>
+export default {
+  components: {
+    HelloWorld,
+  }
+}
+
+<template>
+ <HelloWord />
+  
+</template>
+
+<style lang="scss">
+@use './styles/global.scss'
+
+</style>
