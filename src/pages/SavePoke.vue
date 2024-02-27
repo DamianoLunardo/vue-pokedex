52aa09f4c87841139c90b6dbbccedcb4814c79a3
DamianoL <damianolunardo@hotmail.it>
2024-02-27 14:51:52 +0100

    Save pokemon in local storage

--- src/pages/SavePoke.vue
+++ src/pages/SavePoke.vue
@@ -1,45 +1,57 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 // Variabile per memorizzare i Pokémon salvati
 const savedPokemon = ref([]);
 
+// Carica i dati salvati dal localStorage all'avvio del componente
+onMounted(() => {
+  const savedData = localStorage.getItem('savedPokemon');
+  if (savedData) {
+    savedPokemon.value = JSON.parse(savedData);
+  }
+});
+
+// Funzione per salvare i Pokémon nel localStorage
+const saveToLocalStorage = () => {
+  localStorage.setItem('savedPokemon', JSON.stringify(savedPokemon.value));
+};
+
 // Evento personalizzato per ricevere il nuovo Pokémon salvato
 window.addEventListener('pokemon-saved', (event) => {
   savedPokemon.value.push(event.detail);
+  saveToLocalStorage();
 });
 
-// Funzione per rimuovere un Pokémon dalla lista dei salvati
+// Funzione per rimuovere un Pokémon dalla lista e salvare nel localStorage
 const removePokemon = (index) => {
   savedPokemon.value.splice(index, 1);
+  saveToLocalStorage();
 };
-
-// Variabile per memorizzare il Pokémon selezionato per il dettaglio
-const selectedPokemon = ref(null);
 
 // Funzione per impostare il Pokémon selezionato per visualizzare il dettaglio
 const showPokemonDetail = (pokemon) => {
   const event = new CustomEvent('pokemon-selected', { detail: pokemon });
   window.dispatchEvent(event);
 };
-
 </script>
 
 <template>
   <div>
-        <h1>Ciao</h1>
-        <!-- Mostra le informazioni sui Pokémon salvati -->
-        <div v-for="(pokemon, index) in savedPokemon" :key="index">
-        <div @click="showPokemonDetail(pokemon)">
-            <p>Name: {{ pokemon.name }}</p>
-            <button @click.stop="removePokemon(index)">Remove</button>
-        </div>
-        </div>
+    <h1>My Pokemon</h1>
+    <!-- Mostra le informazioni sui Pokémon salvati -->
+    <div v-for="(pokemon, index) in savedPokemon" :key="index">
+      <div @click="showPokemonDetail(pokemon)">
+        <p> {{ pokemon.name }}</p>
+        <button @click.stop="removePokemon(index)">Remove</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 </style>
+
 
 
 
